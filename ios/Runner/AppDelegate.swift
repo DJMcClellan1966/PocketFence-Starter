0f3b4d5a9d7ec63ffb9c3a5cf0c936820c441604
Douglas McClellan <DJMcClellan1966@gmail.com>
2025-12-14 14:19:20 -0500

    Add iOS device discovery channel (simulator returns a sample device)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,22 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    // Device discovery channel: return simulated list on simulator or empty on device
+    let deviceChannel = FlutterMethodChannel(name: "pocketfence.devices", binaryMessenger: self as! FlutterBinaryMessenger)
+    deviceChannel.setMethodCallHandler { call, result in
+      if call.method == "listDevices" {
+        #if targetEnvironment(simulator)
+        let devices: [[String: String]] = [
+          ["ip": "127.0.0.1", "mac": "00:11:22:33:44:55", "name": "iPhone Simulator", "platform": "ios"]
+        ]
+        result(devices)
+        #else
+        result([])
+        #endif
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
